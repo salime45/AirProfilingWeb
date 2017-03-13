750ad5e5ab78ab2cefc5c73341db5d4347e872f5
Ismael Monje <salime45@gmail.com>
2017-03-13 19:39:02 +0000

    Mejorado formulario de uploader \n Corregido un problema con el favicon en la pantalla de login

--- updater/forms.py
+++ updater/forms.py
@@ -3,5 +3,6 @@ from django import forms
 
 class PcapForm(forms.Form):
     docfile = forms.FileField(
-        label='Seleccione un pcap'
+        label='Seleccione un pcap',
+        help_text='max. 100 megabytes'
     )
