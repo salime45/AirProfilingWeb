69497e79e11fa3d621e50f24baad1e097720349b
Ismael Monje <salime45@gmail.com>
2017-04-14 08:40:47 +0000

    Agregado sdk de tacyt

--- air_profiling/settings.py
+++ air_profiling/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'updater',
     'core',
     'scapy_http',
+    'tacyt',
 ]
 
 MIDDLEWARE = [
--- test.py
+++ test.py
@@ -7,20 +7,18 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "air_profiling.settings")
 django.setup()
 
-from core.models import Link
-from core.integrations import getHost
+import tacyt.tacyt.TacytApp as tacytapp
 
 def test():
 
-        lista = Link.objects.filter(host='')
+    api = tacytapp.TacytApp("PpQbU3AWa773ghLdf2YE", "9UWa3aqaJKrqTkYqtbyUUmyP8uT39NmUYH4HuQWJ")
+    result_search = api.search_apps("links:\"http://www.emtvalencia.es\"",1 , 100, '' ,True)
 
+    list = result_search.data.get('result').get('applications')
 
-
-        for l in  lista:
-            getHost(l.ip_src)
-            getHost(l.ip_dst)
-
-
+    for i in range(len(list)):
+        app = list[i]
+        print (app.get('title'))
 
 test()
 
