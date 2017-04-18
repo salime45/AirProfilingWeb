28457d3beb7e3029381bec3e9e3983b8c3e88881
Ismael Monje <salime45@gmail.com>
2017-04-18 15:32:05 +0000

    Editado el script de backup de la BBDD para que su nombre sea la fecha en la que se hace.

--- scripts/backup-db.sh
+++ scripts/backup-db.sh
@@ -1,6 +1,6 @@
 #mysqldump -u yourusername -h yourusername.mysql.pythonanywhere-services.com 'yourusername$dbname'  > db-backup.sql
-cd /home/imonje/backups
-mysqldump -u imonje -h imonje.mysql.pythonanywhere-services.com 'imonje$air'  > db-backup.sql
+cd ../../backups
+mysqldump -u imonje -h imonje.mysql.pythonanywhere-services.com 'imonje$air'  > "$(date +%F_%R)".sql
 
 #mysql -u yourusername -h yourusername.mysql.pythonanywhere-services.com 'yourusername$dbname'  < db-backup.sql
 
